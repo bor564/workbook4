a2716bfb25a873cca8b194a51661f43807191d4d
bor564 <borronald5@gmail.com>
2023-11-03 14:04:32 -0400

    updates

---
+++ demos/justforunderstanding.js
@@ -0,0 +1,23 @@
+// Set “total” to 0.
+let total = 0
+//  Set “count” to 1.
+let count = 1
+// [loop]
+while (count<=10) {
+    total += count;
+    count += 1
+    
+}
+
+console.log(total);
+    
+
+//  Set “compare” to “count”.
+//  Subtract 11 from “compare”.
+//  If “compare” is zero, continue at [end].
+//  Add “count” to “total”.
+//  Add 1 to “count”.
+//  Continue at [loop].
+// [end]
+//  Output “total”.
+ 
